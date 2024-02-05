703ee10f505784e86050610f0c61af7b453b0a6d
mino <amine222@hotmail.nl>
2024-02-05 21:49:16 +0100

    working on register page: time:4:26:00

--- app/register/page.js
+++ app/register/page.js
@@ -15,12 +15,30 @@ const Register = () => {
     //if authenticated push user to the dashboard
     useEffect(() => {
         if (sessionStatus === "authenticated") {
-            router.push("/")
+            router.push("/dashboard")
         }
-    })
+    }, [sessionStatus, router])
 
-    return (
-        <div>Register</div>
+    return sessionStatus !== "authenticated" && (
+        <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+            <div className="bg-white p-8 rounded shadow-md w-96">
+                <h2 className="text-2xl font-semibold mb-4">
+                    Register
+                </h2>
+
+                <form>
+                    <div className="mb-4">
+                        <div className="mb-4">
+                            <label htmlFor="username" className="block text-gray-700 text-sm font-bold mb-2">
+                                username
+                            </label>
+
+                        </div>
+                    </div>
+                </form>
+
+            </div>
+        </div>
     )
 }
 
